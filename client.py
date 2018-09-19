2551a64d3093e1185cb82350d2f410322b93c63d
faelsfernandes <faelsfernandes@gmail.com>
2018-09-19 10:15:01 -0300

    Atualiação

--- client.py
+++ client.py
@@ -3,9 +3,6 @@ import socket
 import random
 import request_pb2 as request
 import response_pb2 as response
-import communication	
-import hashlib
-import hmac
 import server
 
 def availableFuncHelp():
@@ -16,66 +13,77 @@ def availableFuncHelp():
 	print("EXIT - Close connection")
 
 def createConnection(IP, PORT):
+	#Cria o socket e tenta conectar
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		sock.connect(IP, int(PORT))
+		sock.connect((IP, int(PORT)))
 		print("Connection successfully established!")
-	except: ConnectionRefusedError:
+	except ConnectionRefusedError:
 		print("Could not connect! Exiting...")
 		exit(1)
 
-	request = request.Request()
+	#Pega o comando do usuário
 	command = input("Type your command:").upper()
-	availableFuncHelp()
-	while(command != "EXIT")
+	availableFuncHelp() #Mostra funções disponíveis
+	while(command != "EXIT"):
 		req = request.Request()
 		req.command = command
-		req.upper input("URL: ")
-		req.cId = 3
+		req.url = input("URL: ").upper()
+		req.cId = 3 #Ainda está fix o ID do cliente
 
-		if((req.command == "GET") or (req.command == "DELETE"))
+		#Se for GET ou DELETE, não tem conteúdo na requisição
+		if((req.command == "GET") or (req.command == "DELETE")):
 			message.content = ""
-		else:
+		else: #Se for post, busca o arquivo e coloca o seu conteúdo na requisição
 			if(os.path.exists(req.url)):
 				file = open(req.url, 'r')
 				req.content += file.read()
 				file.close()
-
-		signature = req.command + req.pVersion + req.url + req.cId + req.cInfo + req.encoding + req.content
-		
-
-		req.signature = hmac.new(b'passwd', signature)
-
+		#Faz o HMAC da requisição
+		req.signature = server.clientHMAC(req)
+		#Envia a requisição
 		server.sendRequest(sock, req)
-
+		#Pega a resposta do servidor
 		resp = server.receiveResponse(sock, response.Response())
-		
-		if resp:
-			signature = resp.status + resp.pVersion + resp.url + resp.sInfo + resp.encoding + resp.content + resp.signature
-
-			if signature == resp.signature:
-				print("\n Server response")
-				if req.command == "GET":
-					print("Status:", resp.status)
-					if("OK" in resp.status)
-						print("Content:")
-						print(resp.content)
-					else:
-						print("File not found")
-				elif req.command = "POST":
-					print("Status:", resp.status)
-					if("OK" in resp.status):
-						print("File {0} was created!".format(resp.url))
-					else:
-						print("Error creating file!")
-				elif req.comman == "DELETE":
-					print("Status:", resp.status)
-					if("OK" in resp.status):
-						print("File {0} was successful deleated!")
-					else:
-						print("Error deleting file!")
+	
+		#Faz HMAC do servidor
+		signature = server.serverHMAC(resp)
+		#Se for igual, houve resposta então mostra resultado
+		if signature == resp.signature:
+			print("\n Server response")
+			if req.command == "GET":
+				#print("Status:", resp.status)
+				if("OK" in resp.status):
+					print("Content:")
+					print(resp.content)
 				else:
-					print("Status:", resp.status)
-					print("Command not found!")
-			print("_______New Request______")
-			command = input("Type your command:").upper()
+					print("File not found")
+			elif req.command == "POST":
+				#print("Status:", resp.status)
+				if("OK" in resp.status):
+					print("File {0} was created!".format(resp.url))
+				else:
+					print("Error creating file!")
+			elif req.comman == "DELETE":
+				#print("Status:", resp.status)
+				if("OK" in resp.status):
+					print("File {0} was successful deleated!")
+				else:
+					print("Error deleting file!")
+			else:
+				#print("Status:", resp.status)
+				print("Command not found!")
+		#Ao final, mostra as opções para fazer uma nova requisição
+		availableFuncHelp()
+		print("_______New Request______")
+		command = input("Type your command:").upper()
+
+def main(argv):
+    IP, PORT = argv
+    createConnection(IP, PORT)
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 3):
+        print("Usage: {0} <Ip> <Port>".format(sys.argv[0]))
+        exit(1)
+    main(sys.argv[1:])
--- server.py
+++ server.py
@@ -1,50 +1,74 @@
-import socket, os, sys
-import request_pb2.py as request
-import response_pb2.py as response
+import os, sys, getopt
+import socket
+import threading
+import logging
+import hmac
+import hashlib
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(threadName)s:%(message)s')
+
+import request_pb2 as request
+import response_pb2 as response
+import communication
+from pathlib import Path
 from communication import (
     send_message, recv_message
 )
 
+def clientHMAC(request):
+    data = requestcommand + requestpVersion + requesturl + requestcId + requestcInfo + requestencoding + requestcontent
+    return str(hmac.new(b'passwd', data).hexdigest())
+
+def serverHMAC(request):
+    data = signature = response.status + response.pVersion + response.url + response.sInfo + response.encoding + response.content + response.signature
+    return str(hmac.new(b'passwd', data).hexdigest())
+
+def connect(connection, address):
+    while 1:
+        #request = receiveRequest(connection, request.Request())
+        pass
+        #if request:
+         #   signature = hmac.new(b'passwd', signature)
+          #  if signature == request:
+           #     pass
 
 def sendRequest(sock, request):
     data = request.SerializeToString()
-    size = encodeVarint(len(data))
+    size = struct.pack('>L', len(data))
     sock.sendall(size + data)
 
-def receiveRequest(sock, protoType):
-    data = b''
-    while True:
-        try:
-            data += sock.recv(1)
-            size = decodeVarint(data)
-            break
-        except IndexError:
-            pass
-    data = sock.recv(size)
-    message = protoType()
-    message.ParseFromString(data)
-
+def receiveRequest(sock, request):
+    while 1:
+        print("Listening...")
+        buf_len = sock.recv(4)
+        msg_len = struct.unpack('>L', buf_len)[0]
+        msg_buf = sock.recv(msg_len)
+        message = request()
+        message.ParseFromString(msg_buf)
+        print (message)
+        break
     return message
 
 def createServer(IP, PORT):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            sock.bind(IP, PORT)
-            sock.listen(1)
+            sock.bind((IP, int(PORT))) 
+            sock.listen(10)
         except:
-            logging.info("Erro ao abrir o servidor")
-        logging.info("Servidor aberto na prota {0}".format(PORT))
-        while True:
+            logging.info("Fail to open server")
+        logging.info("Server open in {0}".format(PORT))
+        while 1:
             connection, address = sock.accept()
             threading.Thread(target=connect, args=(connection, address)).start()
         sock.close()
     except (KeyboardInterrupt, SystemExit):
-        logging.info("Servidor finalizado")
+        logging.info("Server closed")
         pass
             
 def main(argv):
-    criaConec(argv)
+    IP, PORT = argv
+    createServer(IP, PORT)
 
 if __name__ == '__main__':
     if (len(sys.argv) != 3):
