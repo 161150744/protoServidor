3e1559d1f12e2004c3b7937153abaa4c0d3fdfe4
faelsfernandes <faelsfernandes@gmail.com>
2018-09-19 18:47:15 -0300

    Função connect semi-pronta

--- server.py
+++ server.py
@@ -5,32 +5,40 @@ import logging
 import hmac
 import hashlib
 
+#Não sei pra que serve isso direito
 logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(threadName)s:%(message)s')
 
 import request_pb2 as request
 import response_pb2 as response
-import communication
-from pathlib import Path
-from communication import (
-    send_message, recv_message
-)
 
+#Tira o HMAC do cliente
 def clientHMAC(request):
     data = requestcommand + requestpVersion + requesturl + requestcId + requestcInfo + requestencoding + requestcontent
     return str(hmac.new(b'passwd', data).hexdigest())
-
+#Tira o HMAC do server
 def serverHMAC(request):
     data = signature = response.status + response.pVersion + response.url + response.sInfo + response.encoding + response.content + response.signature
-    return str(hmac.new(b'passwd', data).hexdigest())
+    return str(hmac.new(b'passwd', data).hexdigest())   
 
+#Função que se conecta
 def connect(connection, address):
     while 1:
-        #request = receiveRequest(connection, request.Request())
-        pass
-        #if request:
-         #   signature = hmac.new(b'passwd', signature)
-          #  if signature == request:
-           #     pass
+        request = receiveRequest(connection, request.Request())
+       
+        signature = hmac.new(b'passwd', signature)
+        if signature == request.signature:
+            if request.command == "DELETE":
+                #response = chamaFunDel()
+                sendRequest(connection, response)
+            else if request.command == "GET":
+                #response = chamaFunGet()
+                sendRequest(connection, response)
+            else if request.command == "POST":
+                #response = chamaFunPost()
+                sendRequest(connection, response)
+            else:
+                #funError()
+    connection.close()
 
 def sendRequest(sock, request):
     data = request.SerializeToString()
@@ -46,18 +54,22 @@ def receiveRequest(sock, request):
         message = request()
         message.ParseFromString(msg_buf)
         print (message)
-        break
+        
     return message
 
+#Função que cria o server
 def createServer(IP, PORT):
+    #Cria um socket
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Vincula o socket a um endereço
         try:
             sock.bind((IP, int(PORT))) 
             sock.listen(10)
         except:
             logging.info("Fail to open server")
         logging.info("Server open in {0}".format(PORT))
+        #Chama a função que contém as opções de requisição
         while 1:
             connection, address = sock.accept()
             threading.Thread(target=connect, args=(connection, address)).start()
